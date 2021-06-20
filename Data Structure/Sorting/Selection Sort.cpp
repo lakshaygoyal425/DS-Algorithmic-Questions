32b4bb797e76a7ba92cd9d82a147461c633b9c8a
Lakshay Goyal <47741067+lakshaygoyal425@users.noreply.github.com>
2021-06-20 10:14:44 +0530

    Create Selection Sort.cpp

---
+++ Data Structure/Sorting/Selection Sort.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int SelectionSort(int arr[], int n){
+	for(int i=0;i<n;i++){
+		int min = i;
+		for(int j=i+1;j<n;j++){
+			if(arr[j] < arr[min]){
+				min = j;
+			}
+		}
+		swap(arr[min], arr[i]);
+	}
+}
+
+
+int main()
+{
+	int arr[] = {15, 36, 7, 38, 10, 20, 68};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	SelectionSort(arr, n);	
+	for(int i = 0;i < n; i++){
+	    cout<<arr[i]<<" ";
+	}
+}
